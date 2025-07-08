313d052ac64e8c2680ae8acfdcb3605395890d82
aryanpatel1918 <aryanpatel1918@gmail.com>
2025-07-08 15:24:31 -0400

    Added functionality and improved CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
 
 export default function App() {
-  const [color, setColor] = useState("yellow")
+  const [color, setColor] = useState("")
 
   return (
     <>
       <div class="square" style={{ backgroundColor: color }}>
         <span>{color}</span>
       </div>
-      <input id="input-el" onChange={(e) => setColor(e.target.value)} type="text" />
+      
+      <label>
+        <input
+          id="input-el"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+          type="text"
+          placeholder="Enter a color"
+        />
+      </label>
     </>
   )
 }
